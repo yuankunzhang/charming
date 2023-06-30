4e82ca3e7020f741ecc0a08e99eac2c6e22f100d
yuankunzhang <fmcocoa@gmail.com>
2023-06-30 23:18:48 +0800

    SankeySeries

--- src/component/emphasis.rs
+++ src/component/emphasis.rs
@@ -10,6 +10,7 @@ pub enum Focus {
     Ancestor,
     Descendant,
     Relative,
+    Adjacency,
 }
 
 #[derive(Serialize)]
--- src/series/sankey.rs
+++ src/series/sankey.rs
@@ -1,5 +1,175 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize, __private::de};
+
+use crate::component::{emphasis::Emphasis, line_style::LineStyle};
+
+#[derive(Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum NodeAlign {
+    Left,
+    Right,
+    Justify,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Node {
+    pub name: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub value: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub depth: Option<u64>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Link {
+    pub source: String,
+    pub target: String,
+    pub value: f64,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Data {
+    pub nodes: Vec<Node>,
+    pub links: Vec<Link>,
+}
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Sankey {}
+pub struct Sankey {
+    #[serde(rename = "type")]
+    type_: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    z_level: Option<u64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    z: Option<u64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    left: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    top: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    right: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    bottom: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    width: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    height: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    emphasis: Option<Emphasis>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    node_align: Option<NodeAlign>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    line_style: Option<LineStyle>,
+
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    links: Vec<Link>,
+
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    data: Vec<Node>,
+}
+
+impl Sankey {
+    pub fn new() -> Sankey {
+        Sankey {
+            type_: "sankey".to_string(),
+            name: None,
+            z_level: None,
+            z: None,
+            left: None,
+            top: None,
+            right: None,
+            bottom: None,
+            width: None,
+            height: None,
+            emphasis: None,
+            node_align: None,
+            line_style: None,
+            links: vec![],
+            data: vec![],
+        }
+    }
+
+    pub fn name<S: Into<String>>(mut self, name: S) -> Sankey {
+        self.name = Some(name.into());
+        self
+    }
+
+    pub fn z_level(mut self, z_level: u64) -> Self {
+        self.z_level = Some(z_level);
+        self
+    }
+
+    pub fn z(mut self, z: u64) -> Self {
+        self.z = Some(z);
+        self
+    }
+
+    pub fn left<S: Into<String>>(mut self, left: S) -> Self {
+        self.left = Some(left.into());
+        self
+    }
+
+    pub fn top<S: Into<String>>(mut self, top: S) -> Self {
+        self.top = Some(top.into());
+        self
+    }
+
+    pub fn right<S: Into<String>>(mut self, right: S) -> Self {
+        self.right = Some(right.into());
+        self
+    }
+
+    pub fn bottom<S: Into<String>>(mut self, bottom: S) -> Self {
+        self.bottom = Some(bottom.into());
+        self
+    }
+
+    pub fn width(mut self, width: f64) -> Self {
+        self.width = Some(width);
+        self
+    }
+
+    pub fn height(mut self, height: f64) -> Self {
+        self.height = Some(height);
+        self
+    }
+
+    pub fn emphasis(mut self, emphasis: Emphasis) -> Self {
+        self.emphasis = Some(emphasis);
+        self
+    }
+
+    pub fn node_align(mut self, node_align: NodeAlign) -> Self {
+        self.node_align = Some(node_align);
+        self
+    }
+
+    pub fn line_style(mut self, line_style: LineStyle) -> Self {
+        self.line_style = Some(line_style);
+        self
+    }
+
+    pub fn data(mut self, data: Data) -> Self {
+        self.links = data.links;
+        self.data = data.nodes;
+        self
+    }
+}
