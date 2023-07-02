95ae4c08e863739f22fa7fe9d039e9fc3a33bb5c
yuankunzhang <fmcocoa@gmail.com>
2023-07-02 20:36:02 +0800

    basic_scatter_chart.rs

--- src/series/scatter.rs
+++ src/series/scatter.rs
@@ -1,6 +1,10 @@
 use serde::Serialize;
 
-use crate::basic::color::ColorBy;
+use crate::basic::{
+    color::ColorBy, coordinate::CoordinateSystem, symbol::Symbol, DataFrame, DataPoint,
+};
+
+pub type Data = DataFrame;
 
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -15,7 +19,24 @@ pub struct Scatter {
     color_by: Option<ColorBy>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    dataset_index: Option<u64>,
+    dataset_index: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    coordinate_system: Option<CoordinateSystem>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    x_axis_index: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    y_axis_index: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    symbol: Option<Symbol>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    symbol_size: Option<f64>,
+
+    data: Data,
 }
 
 impl Scatter {
@@ -25,6 +46,12 @@ impl Scatter {
             name: None,
             color_by: None,
             dataset_index: None,
+            coordinate_system: None,
+            x_axis_index: None,
+            y_axis_index: None,
+            symbol: None,
+            symbol_size: None,
+            data: vec![],
         }
     }
 
@@ -38,8 +65,38 @@ impl Scatter {
         self
     }
 
-    pub fn dataset_index(mut self, dataset_index: u64) -> Self {
-        self.dataset_index = Some(dataset_index);
+    pub fn dataset_index<F: Into<f64>>(mut self, dataset_index: F) -> Self {
+        self.dataset_index = Some(dataset_index.into());
+        self
+    }
+
+    pub fn coordinate_system(mut self, coordinate_system: CoordinateSystem) -> Self {
+        self.coordinate_system = Some(coordinate_system);
+        self
+    }
+
+    pub fn x_axis_index<F: Into<f64>>(mut self, x_axis_index: F) -> Self {
+        self.x_axis_index = Some(x_axis_index.into());
+        self
+    }
+
+    pub fn y_axis_index<F: Into<f64>>(mut self, y_axis_index: F) -> Self {
+        self.y_axis_index = Some(y_axis_index.into());
+        self
+    }
+
+    pub fn symbol(mut self, symbol: Symbol) -> Self {
+        self.symbol = Some(symbol);
+        self
+    }
+
+    pub fn symbol_size<F: Into<f64>>(mut self, symbol_size: F) -> Self {
+        self.symbol_size = Some(symbol_size.into());
+        self
+    }
+
+    pub fn data<D: Into<DataPoint>>(mut self, data: Vec<D>) -> Self {
+        self.data = data.into_iter().map(|d| d.into()).collect();
         self
     }
 }
