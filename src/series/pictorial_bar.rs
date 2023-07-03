05198759311f864870d9865c06a47cedc878a9ce
yuankunzhang <fmcocoa@gmail.com>
2023-07-03 23:14:30 +0800

    PictorialBar

--- src/series/pictorial_bar.rs
+++ src/series/pictorial_bar.rs
@@ -1,5 +1,53 @@
 use serde::Serialize;
 
+use crate::element::{
+    color::ColorBy,
+    coordinate::CoordinateSystem,
+    cursor::Cursor,
+    emphasis::Emphasis,
+    item_style::ItemStyle,
+    label::{Label, LabelLayout, LabelLine},
+};
+
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase")]
-pub struct PictorialBar {}
+pub struct PictorialBar {
+    #[serde(rename = "type")]
+    type_: String,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    color_by: Option<ColorBy>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    legend_hover_link: Option<bool>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    coordinate_system: Option<CoordinateSystem>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    x_axis_index: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    y_axis_index: Option<f64>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    cursor: Option<Cursor>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    label: Option<Label>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    label_line: Option<LabelLine>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    label_layout: Option<LabelLayout>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    item_style: Option<ItemStyle>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    emphasis: Option<Emphasis>,
+}
