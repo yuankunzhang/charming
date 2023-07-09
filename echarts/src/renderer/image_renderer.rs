45476a03fd46dd21935a6acd82774c02168a900a
yuankunzhang <fmcocoa@gmail.com>
2023-07-09 12:53:33 +0800

    Render to different image formats

--- echarts/src/renderer/image_renderer.rs
+++ echarts/src/renderer/image_renderer.rs
@@ -1,4 +1,5 @@
 use deno_core::{v8, JsRuntime, RuntimeOptions};
+use gdk_pixbuf::traits::PixbufLoaderExt;
 use handlebars::Handlebars;
 
 use crate::Chart;
@@ -15,6 +16,22 @@ chart.setOption({ animation: false });
 chart.setOption({{{ chart_option }}});
 chart.renderToSVGString();
 "#;
+
+pub enum ImageFormat {
+    SVG,
+    PNG,
+    JPEG,
+}
+
+impl ToString for ImageFormat {
+    fn to_string(&self) -> String {
+        match self {
+            ImageFormat::SVG => "svg".to_string(),
+            ImageFormat::PNG => "png".to_string(),
+            ImageFormat::JPEG => "jpeg".to_string(),
+        }
+    }
+}
 
 pub struct ImageRenderer {
     js_runtime: JsRuntime,
@@ -47,7 +64,7 @@ impl ImageRenderer {
         }
     }
 
-    pub fn render(mut self, chart: Chart) -> String {
+    pub fn render(&mut self, chart: &Chart) -> String {
         let code = Handlebars::new()
             .render_template(
                 CODE_TEMPLATE,
@@ -73,5 +90,26 @@ impl ImageRenderer {
             }
             Err(error) => panic!("{}", error.to_string()),
         }
+    }
+
+    pub fn render_format(&mut self, chart: &Chart, fmt: ImageFormat) -> Vec<u8> {
+        let svg = self.render(chart);
+
+        let loader = gdk_pixbuf::PixbufLoader::with_mime_type("image/svg+xml").unwrap();
+        loader.write(svg.as_bytes()).unwrap();
+        loader.close().unwrap();
+
+        let pixbuf = loader.pixbuf().unwrap();
+        pixbuf.save_to_bufferv(&fmt.to_string(), &[]).unwrap()
+    }
+
+    pub fn save(&mut self, chart: &Chart, path: &str) {
+        let svg = self.render(chart);
+        std::fs::write(path, svg).unwrap();
+    }
+
+    pub fn save_format(&mut self, chart: &Chart, fmt: ImageFormat, path: &str) {
+        let bytes = self.render_format(chart, fmt);
+        std::fs::write(path, bytes).unwrap();
     }
 }
