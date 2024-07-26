164e2be12b8ccdca36bbc4685100a6297b2537e5
v3xro <175618876+v3xro@users.noreply.github.com>
2024-07-26 17:02:33 +0100

    feat: allow using Formatter::Function from WASM, add value_formatter in Tooltip

--- charming/src/element/formatter.rs
+++ charming/src/element/formatter.rs
@@ -1,11 +1,15 @@
-use serde::Serialize;
-
 use super::RawString;
+use serde::Serialize;
 
 #[derive(Serialize)]
 #[serde(untagged)]
 pub enum Formatter {
     String(String),
+
+    #[cfg(target_arch = "wasm32")]
+    Function(#[serde(with = "serde_wasm_bindgen::preserve")] web_sys::js_sys::Function),
+
+    #[cfg(not(target_arch = "wasm32"))]
     Function(RawString),
 }
 
@@ -15,6 +19,7 @@ impl From<&str> for Formatter {
     }
 }
 
+#[cfg(not(target_arch = "wasm32"))]
 impl From<RawString> for Formatter {
     fn from(s: RawString) -> Self {
         Formatter::Function(s)
