413e836bf30bf11bbf4883040a557236500a30cc
humphreylee <66808535+humphreylee@users.noreply.github.com>
2024-10-24 11:35:00 +0800

    Added dz! test

--- charming/tests/dz_test.rs
+++ charming/tests/dz_test.rs
@@ -1,30 +1,9 @@
-/*
-use charming::datatype::DataFrame;
-use charming::dz;
-
-use charming::component::Type::Value;
-use charming::datatype::DataPoint::Value;
-use charming::element::AxisType::Value;
-use charming::element::Color::Value;
-use charming::datatype::CompositeValue::Array;
-use serde_json::Value::Array;
-use charming::datatype::CompositeValue::String;
-use charming::element::Formatter::String;
-use serde_json::Value::String;
-use charming::datatype::NumericValue::Integer;
-use charming::datatype::CompositeValue::Number;
-use charming::datatype::DimensionType::Number;
-use charming::element::SymbolSize::Number;
-use serde_json::Value::Number;
-use charming::datatype::NumericValue::Integer;
-use charming::datatype::DimensionType::Float;
-use charming::datatype::NumericValue::Float;
-*/
-
 #[cfg(test)]
 mod tests {
 
-    fn dz_macro_case_1() {
+    use charming::{dz, df};
+    #[test]
+    fn dz_test1() {
         // Set up test data
         let d1 = vec![44056, 13334];
         let d2 = vec![81.8, 76.9];
@@ -33,12 +12,15 @@ mod tests {
         let d5 = vec![2015, 2015];
 
         // expected output
-        let expected_output: DataFrame = [Value(Array([Number(Integer(44056)), Number(Float(81.8)), Number(Integer(23968973)), String("Australia"), Number(Integer(2015))])), Value(Array([Number(Integer(13334)), Number(Float(76.9)), Number(Integer(1376048943)), String("China"), Number(Integer(2015))]))];
+        let df_out = df![
+            [44056, 81.8, 23968973, "Australia", 2015],
+            [13334, 76.9, 1376048943, "China", 2015]
+        ];
 
         // Call the macro
-        let df = dz!(d1, d2, d3, d4, d5);
+        let dz_out = dz!(d1, d2, d3, d4, d5);
 
         // Assert expected output
-        assert_eq!(df, expected_output);
+        assert_eq!(dz_out, df_out);
     }
 }
