969c08df3c571c5b30c75a60f3abde85f42bb98b
LukaOber <lukaobermayr@gmail.com>
2025-05-17 10:43:20 +0200

    seperate js_function from formatter

--- charming/src/element/formatter.rs
+++ charming/src/element/formatter.rs
@@ -1,36 +1,11 @@
+use crate::element::js_function::JsFunction;
 use serde::Serialize;
-
-#[derive(Serialize, Debug, PartialEq, Clone)]
-#[serde(transparent)]
-pub struct FormatterFunction {
-    #[cfg(not(target_arch = "wasm32"))]
-    value: super::RawString,
-    #[cfg(target_arch = "wasm32")]
-    #[serde(with = "serde_wasm_bindgen::preserve")]
-    value: js_sys::Function,
-}
-
-impl FormatterFunction {
-    #[cfg(not(target_arch = "wasm32"))]
-    pub fn new_with_args(args: &str, body: &str) -> FormatterFunction {
-        FormatterFunction {
-            value: super::RawString::from(format!("function({}) {{ {} }}", args, body)),
-        }
-    }
-
-    #[cfg(target_arch = "wasm32")]
-    pub fn new_with_args(args: &str, body: &str) -> FormatterFunction {
-        FormatterFunction {
-            value: js_sys::Function::new_with_args(args, body),
-        }
-    }
-}
 
 #[derive(Serialize, Debug, PartialEq, Clone)]
 #[serde(untagged)]
 pub enum Formatter {
     String(String),
-    Function(FormatterFunction),
+    Function(JsFunction),
 }
 
 impl From<&str> for Formatter {
@@ -39,8 +14,8 @@ impl From<&str> for Formatter {
     }
 }
 
-impl From<FormatterFunction> for Formatter {
-    fn from(f: FormatterFunction) -> Self {
+impl From<JsFunction> for Formatter {
+    fn from(f: JsFunction) -> Self {
         Formatter::Function(f)
     }
 }
