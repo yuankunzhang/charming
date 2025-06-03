6eabbaf27d185029b1be3c96fc40bd6c1474e063
Frederic Taillandier <frederic.taillandier@advertima.com>
2025-06-03 14:17:36 +0200

    fix: leptos example code complaining about non reactivity

--- examples/leptos-demo/src/main.rs
+++ examples/leptos-demo/src/main.rs
@@ -8,37 +8,33 @@ use leptos::prelude::*;
 use leptos_use::use_interval_fn;
 use leptos_use::utils::Pausable;
 
+
 #[component]
 fn App() -> impl IntoView {
     let data = RwSignal::new(vec![150, 230, 224, 218, 135, 147, 260]);
-    let action = Action::new(move |_input: &()| {
-        let local = data.get();
-        async move {
-            let chart = Chart::new()
-                .title(Title::new().text("Demo: Leptos + Charming"))
-                .x_axis(
-                    Axis::new()
-                        .type_(AxisType::Category)
-                        .data(vec!["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]),
-                )
-                .y_axis(Axis::new().type_(AxisType::Value))
-                .series(Line::new().data(local));
 
-            let renderer = WasmRenderer::new(600, 400);
-            renderer.render("chart",&chart).unwrap();
-    }});
+    Effect::new(move |_| {
+        let local = data.get();
+
+        let chart = Chart::new()
+            .title(Title::new().text("Demo: Leptos + Charming"))
+            .x_axis(
+                Axis::new()
+                    .type_(AxisType::Category)
+                    .data(vec!["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]),
+            )
+            .y_axis(Axis::new().type_(AxisType::Value))
+            .series(Line::new().data(local));
+
+        let renderer = WasmRenderer::new(600, 400);
+        renderer.render("chart", &chart).unwrap();
+    });
 
     let Pausable { pause, resume, is_active: _ } = use_interval_fn(
         move || {
             data.update(|d| d.rotate_right(1));
-            action.dispatch(());
-        },
-        1000
+        }, 1000
     );
-    
-
-    action.dispatch(());
-
     view! {
         <div>
             <div id="chart"></div>
