8baaffd38cb9bdf9ce067eb5ae47ea740d70f0ee
LukaOber <lukaobermayr@gmail.com>
2025-06-06 05:23:06 +0200

    use derive macro on datatypes

--- charming/src/datatype/dataset.rs
+++ charming/src/datatype/dataset.rs
@@ -1,8 +1,7 @@
-use serde::{de::Visitor, ser::SerializeSeq, Deserialize, Deserializer, Serialize};
-
-use crate::element::RawString;
-
 use super::{DataSource, Dimension};
+use crate::element::RawString;
+use charming_macros::CharmingSetters;
+use serde::{de::Visitor, ser::SerializeSeq, Deserialize, Deserializer, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, PartialOrd, Clone)]
 pub struct Source {
@@ -63,66 +62,18 @@ where
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, PartialOrd, Clone)]
+#[serde_with::apply(
+  Option => #[serde(skip_serializing_if = "Option::is_none")],
+  Vec => #[serde(default, skip_serializing_if = "Vec::is_empty")]
+)]
+#[derive(Serialize, Deserialize, CharmingSetters, Debug, PartialEq, PartialOrd, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Transform {
-    #[serde(skip_serializing_if = "Option::is_none")]
     id: Option<String>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
     transform: Option<RawString>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
     from_dataset_id: Option<String>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
     from_dataset_index: Option<i32>,
-
-    #[serde(skip_serializing_if = "Option::is_none")]
     from_transform_result: Option<i32>,
-}
-
-impl Default for Transform {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl Transform {
-    pub fn new() -> Self {
-        Self {
-            id: None,
-            transform: None,
-            from_dataset_id: None,
-            from_dataset_index: None,
-            from_transform_result: None,
-        }
-    }
-
-    pub fn id<S: Into<String>>(mut self, id: S) -> Self {
-        self.id = Some(id.into());
-        self
-    }
-
-    pub fn transform<R: Into<RawString>>(mut self, transform: R) -> Self {
-        self.transform = Some(transform.into());
-        self
-    }
-
-    pub fn from_dataset_id<S: Into<String>>(mut self, from_dataset_id: S) -> Self {
-        self.from_dataset_id = Some(from_dataset_id.into());
-        self
-    }
-
-    pub fn from_dataset_index<I: Into<i32>>(mut self, from_dataset_index: I) -> Self {
-        self.from_dataset_index = Some(from_dataset_index.into());
-        self
-    }
-
-    pub fn from_transform_result<I: Into<i32>>(mut self, from_transform_result: I) -> Self {
-        self.from_transform_result = Some(from_transform_result.into());
-        self
-    }
 }
 
 impl From<&str> for Transform {
