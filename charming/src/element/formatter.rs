f67d7ba0c217a1eb7fbbebd25be61950da30ab3b
LukaOber <lukaobermayr@gmail.com>
2025-06-13 11:49:34 +0200

    remove formatterfunction

--- charming/src/element/formatter.rs
+++ charming/src/element/formatter.rs
@@ -2,32 +2,6 @@ use crate::element::js_function::JsFunction;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
-#[serde(transparent)]
-pub struct FormatterFunction {
-    #[cfg(not(target_arch = "wasm32"))]
-    value: super::RawString,
-    #[cfg(target_arch = "wasm32")]
-    #[serde(with = "serde_wasm_bindgen::preserve")]
-    value: js_sys::Function,
-}
-
-impl FormatterFunction {
-    #[cfg(not(target_arch = "wasm32"))]
-    pub fn new_with_args(args: &str, body: &str) -> FormatterFunction {
-        FormatterFunction {
-            value: super::RawString::from(format!("function({}) {{ {} }}", args, body)),
-        }
-    }
-
-    #[cfg(target_arch = "wasm32")]
-    pub fn new_with_args(args: &str, body: &str) -> FormatterFunction {
-        FormatterFunction {
-            value: js_sys::Function::new_with_args(args, body),
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 #[serde(untagged)]
 pub enum Formatter {
     String(String),
